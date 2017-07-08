257f9c3c26426ec7a74d93dc9e503b1533b26fca
RikSchoonbeek <rik.schoonbeek@gmail.com>
2017-07-08 21:55:34 +0200

    Changed input from int to str for easier handling

--- mastermind.py
+++ mastermind.py
@@ -1,47 +1,51 @@
 # ------ TO DO ------
 # * user can still give faulty input by entering a float, this has to be taken care of @ GetInput
+# Change input type from int to string for easier handling
 
 from random import randint
 
 
 # Determine numbers to be guessed
-def DetermineNumbers():
-    x = ''
+def determineNumbers():
+    number = ''
     for i in range(4):
-        x += str(randint(0,9))
+        number += str(randint(0,9))
         
 
 # Get user input, make sure it is a 4 digit int and communicate with user
-def GetInput():
+def getInput():
     message = ''
     intCheck = False
     digitCheck = False
     while intCheck is False or digitCheck is False:
-        try:
-            guess = int(input("Enter your guess here: "))
-            intCheck = True
-        except ValueError:
-            print('Your input can only exist of the numbers 0 to 9, no other characters are allowed.')
-            intCheck = False
-
-        if len(str(guess)) != 4:
-            print('You entered ' + str(len(str(guess))) + ' digits, please enter four.')
+        guess = input("Enter your guess here: ")
+        if len(guess) != 4:
+            print('You entered ' + str(len(guess)) + ' digits, please enter 4.')
             digitCheck = False
         else:
             digitCheck = True
+
+        for i in guess:
+            if i not in '0123456789':
+                intCheck = False
+            else:
+                intCheck = True
+
+        if intCheck is False:
+                print('Your input can only exist of the numbers 0 to 9, no other characters are allowed.')
             
     return guess
 
 
 # Check user input
-def CheckUserInput():
-    guess = GetInput()
+def checkUserInput():
+    guess = getInput()
     print(guess)
 
 # Toon gegokte nummer en resultaten van vorige gok
-def ShowOutput():
+def showOutput():
     pass
 
 
-DetermineNumbers()
-CheckUserInput()
+determineNumbers()
+checkUserInput()
